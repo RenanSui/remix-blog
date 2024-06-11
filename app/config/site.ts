95b2a7c348c40922f37dd621bb70664a4a177765
RenanSui <renanddtao@gmail.com>
2024-06-11 10:07:23 -0300

    create site config

---
+++ app/config/site.ts
@@ -0,0 +1,45 @@
+import { Navbar } from '~/types'
+
+const navbar: Navbar = {
+  items: [
+    {
+      title: 'Home',
+      href: '/',
+      icon: 'home',
+      disabled: false,
+    },
+    {
+      title: 'Search',
+      href: '/post/search',
+      icon: 'home',
+      disabled: false,
+    },
+    {
+      title: 'Profile',
+      href: '/profile',
+      icon: 'home',
+      disabled: false,
+    },
+    {
+      title: 'Settings',
+      href: '/dashboard/settings',
+      icon: 'home',
+      disabled: false,
+    },
+  ],
+}
+
+const dashboard: Navbar = {
+  items: [],
+}
+
+export const siteConfig = {
+  name: 'Blog',
+  title: 'Sui Blog',
+  description: 'Simple blog created with Remix.',
+  // links,
+  // unsplash: { auth },
+  // oauthProviders,
+  sidebarNav: [...navbar.items],
+  dashboardNav: [...dashboard.items],
+}
