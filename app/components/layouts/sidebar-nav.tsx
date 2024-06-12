5bb3beee9b9cc52b14c3aeb3099e6098600462a5
RenanSui <renanddtao@gmail.com>
2024-06-12 12:00:39 -0300

    fix -> imports, modify -> sidebar style

--- app/components/layouts/sidebar-nav.tsx
+++ app/components/layouts/sidebar-nav.tsx
@@ -1,39 +1,66 @@
 import { cn } from '@/lib/utils'
-import { SidebarNavItem } from '@/types'
+import type { SidebarNavItem } from '@/types'
 import { Link } from '@remix-run/react'
 import { Icons } from '../icon'
 import { buttonVariants } from '../ui/button'
 
 export interface SidebarNavProps extends React.HTMLAttributes<HTMLDivElement> {
   items: SidebarNavItem[]
+  isCollapsed?: boolean
 }
 
-export function SidebarNav({ items, className, ...props }: SidebarNavProps) {
+export function SidebarNav({
+  items,
+  isCollapsed,
+  className,
+  ...props
+}: SidebarNavProps) {
   if (!items?.length) return null
 
   return (
     <div
-      className={cn('flex w-full flex-col gap-1 text-sm', className)}
+      data-collapsed={isCollapsed}
+      className={cn(
+        'group flex flex-col gap-4 text-sm py-2',
+        className,
+      )}
       {...props}
     >
-      {items.map((item, index) => {
-        const Icon = Icons[item.icon ?? 'chevronLeft']
+      <nav className="grid gap-1 group-[[data-collapsed=true]]:justify-center">
+        {items.map((item, index) => {
+          const Icon = Icons[item.icon ?? 'chevronLeft']
 
-        return (
-          <Link aria-label={item.title} key={index} to={item.href}>
-            <span
+          return isCollapsed ? (
+            <Link
+              aria-label={item.title}
+              key={index}
+              to={item.href}
               className={cn(
-                buttonVariants({ variant: 'ghost' }),
-                'group flex justify-start px-3 gap-2',
+                buttonVariants({ variant: 'ghost', size: 'icon' }),
+                'h-9 w-9',
                 item.disabled && 'pointer-events-none opacity-60',
               )}
             >
-              <Icon className="size-4" aria-hidden="true" />
-              <span>{item.title}</span>
-            </span>
-          </Link>
-        )
-      })}
+              <Icon className="h-4 w-4" />
+              <span className="sr-only">{item.title}</span>
+            </Link>
+          ) : (
+            <Link
+              aria-label={item.title}
+              key={index}
+              to={item.href}
+              className={cn(
+                buttonVariants({ variant: 'ghost', size: "sm" }),
+                'w-full',
+                item.disabled && 'pointer-events-none opacity-60',
+              )}
+            >
+              <Icon className="mr-2 h-4 w-4" />
+              <span className='mr-auto'>{item.title}</span>
+            </Link>
+          )
+        })}
+      </nav>
     </div>
   )
 }
