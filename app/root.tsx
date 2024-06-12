e0df32cd53efe33c945c4fbce2f6bb0c1b776169
RenanSui <renanddtao@gmail.com>
2024-06-12 12:11:16 -0300

    modify -> app root

--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,49 @@
-import { LinksFunction } from '@remix-run/node'
+import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node'
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  json,
+  useLoaderData,
 } from '@remix-run/react'
-import { SiteSidebar } from './components/layouts/site-sidebar'
+import SiteLayout from './components/layouts/site-layout'
+import { collapsedCookie, layoutCookie } from './cookies.server'
 import stylesheet from './tailwind.css?url'
 
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: stylesheet },
 ]
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const cookieHeader = request.headers.get('Cookie')
+  const layout = await layoutCookie.parse(cookieHeader)
+  const collapsed = await collapsedCookie.parse(cookieHeader)
+
+  if (layout && collapsed) {
+    return json({ layout, collapsed })
+  }
+
+  return json(
+    { layout, collapsed },
+    {
+      headers: [
+        [
+          'Set-Cookie',
+          await layoutCookie.serialize({ value: [265, 440, 655] }),
+        ],
+        ['Set-Cookie', await collapsedCookie.serialize({ value: false })],
+      ],
+    },
+  )
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { collapsed, layout } = useLoaderData<typeof loader>()
+  const defaultLayout = layout ? layout.value : undefined
+  const defaultCollapsed = collapsed ? layout.value : undefined
+
   return (
     <html lang="en" className="dark">
       <head>
@@ -23,10 +53,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <div className="flex">
-          <SiteSidebar user={{ name: 'Renan Sui' }} />
-          {children}
-        </div>
+        <SiteLayout
+          user={{ name: 'Renan Sui' }}
+          page={children}
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
         <ScrollRestoration />
         <Scripts />
       </body>
