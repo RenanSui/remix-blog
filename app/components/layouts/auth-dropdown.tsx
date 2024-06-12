f7aea86836475d8af033d7f45b49f6e5e76c11e4
RenanSui <renanddtao@gmail.com>
2024-06-12 12:01:08 -0300

    fix -> imports, modify -> auth-dropdown style

--- app/components/layouts/auth-dropdown.tsx
+++ app/components/layouts/auth-dropdown.tsx
@@ -3,7 +3,8 @@ import { ExitIcon } from '@radix-ui/react-icons'
 import { Link } from '@remix-run/react'
 import { ChevronsUpDown } from 'lucide-react'
 import { Icons } from '../icon'
-import { Button, ButtonProps } from '../ui/button'
+import type { ButtonProps } from '../ui/button';
+import { Button } from '../ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,9 +16,15 @@ type User = { name: string }
 
 type AuthDropdownProps = ButtonProps & {
   user: User | null
+  isCollapsed?: boolean
 }
 
-export function AuthDropdown({ user, className, ...props }: AuthDropdownProps) {
+export function AuthDropdown({
+  user,
+  isCollapsed,
+  className,
+  ...props
+}: AuthDropdownProps) {
   if (!user) {
     return (
       <Button
@@ -38,12 +45,23 @@ export function AuthDropdown({ user, className, ...props }: AuthDropdownProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="w-full hover:bg-transparent">
-          <Icons.logo className="size-4 min-h-4 min-w-4 mr-2" />
-          <span className="mr-auto max-w-full overflow-hidden text-ellipsis whitespace-nowrap">
-            {user.name}
+        <Button
+          variant="outline"
+          className={cn(
+            'w-full hover:bg-transparent',
+            'flex items-center gap-2 [&>span]:line-clamp-1 [&>span]:flex [&>span]:w-full [&>span]:items-center [&>span]:gap-1 [&>span]:truncate [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0',
+            isCollapsed &&
+              'flex h-9 w-9 shrink-0 items-center justify-center p-0 [&>span]:w-auto',
+          )}
+        >
+          <span className="flex w-full items-center gap-3 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0 [&_svg]:text-foreground">
+            {/* eslint-disable-next-line react/jsx-pascal-case */}
+            <Icons.logo className="size-4 shrink-0" />
+            {isCollapsed ? null : user.name}
+            <ChevronsUpDown
+              className={cn('ml-auto size-4 shrink-0', isCollapsed && 'hidden')}
+            />
           </span>
-          <ChevronsUpDown className="ml-2 size-4 shrink-0 opacity-50" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-60" align="end" forceMount>
