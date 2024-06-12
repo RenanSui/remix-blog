c8690dfbbee042592a01a072f6566e1486630809
RenanSui <renanddtao@gmail.com>
2024-06-12 12:24:17 -0300

    modify -> sidebar to handle active nav link

--- app/components/layouts/sidebar-nav.tsx
+++ app/components/layouts/sidebar-nav.tsx
@@ -1,12 +1,12 @@
-import { cn } from '@/lib/utils'
-import type { SidebarNavItem } from '@/types'
-import { Link } from '@remix-run/react'
-import { Icons } from '../icon'
-import { buttonVariants } from '../ui/button'
+import { cn } from "@/lib/utils";
+import type { SidebarNavItem } from "@/types";
+import { NavLink } from "@remix-run/react";
+import { Icons } from "../icon";
+import { buttonVariants } from "../ui/button";
 
 export interface SidebarNavProps extends React.HTMLAttributes<HTMLDivElement> {
-  items: SidebarNavItem[]
-  isCollapsed?: boolean
+  items: SidebarNavItem[];
+  isCollapsed?: boolean;
 }
 
 export function SidebarNav({
@@ -15,52 +15,55 @@ export function SidebarNav({
   className,
   ...props
 }: SidebarNavProps) {
-  if (!items?.length) return null
+  if (!items?.length) return null;
 
   return (
     <div
       data-collapsed={isCollapsed}
-      className={cn(
-        'group flex flex-col gap-4 text-sm py-2',
-        className,
-      )}
+      className={cn("group flex flex-col gap-4 text-sm py-2", className)}
       {...props}
     >
       <nav className="grid gap-1 group-[[data-collapsed=true]]:justify-center">
         {items.map((item, index) => {
-          const Icon = Icons[item.icon ?? 'chevronLeft']
+          const Icon = Icons[item.icon ?? "chevronLeft"];
 
           return isCollapsed ? (
-            <Link
+            <NavLink
               aria-label={item.title}
               key={index}
               to={item.href}
-              className={cn(
-                buttonVariants({ variant: 'ghost', size: 'icon' }),
-                'h-9 w-9',
-                item.disabled && 'pointer-events-none opacity-60',
-              )}
+              className={({ isActive, isPending }) => {
+                return cn(
+                  buttonVariants({ variant: "ghost", size: "icon" }),
+                  "h-9 w-9",
+                  isActive ? "bg-accent text-accent-foreground" : "",
+                  item.disabled && "pointer-events-none opacity-60"
+                );
+              }}
             >
               <Icon className="h-4 w-4" />
               <span className="sr-only">{item.title}</span>
-            </Link>
+            </NavLink>
           ) : (
-            <Link
+            <NavLink
               aria-label={item.title}
               key={index}
               to={item.href}
-              className={cn(
-                buttonVariants({ variant: 'ghost', size: "sm" }),
-                'w-full',
-                item.disabled && 'pointer-events-none opacity-60',
-              )}
+              className={({ isActive, isPending }) => {
+                return cn(
+                  buttonVariants({ variant: "ghost", size: "sm" }),
+                  "w-full",
+                  isActive ? "bg-accent text-accent-foreground" : "",
+                  item.disabled && "pointer-events-none opacity-60"
+                );
+              }}
             >
               <Icon className="mr-2 h-4 w-4" />
-              <span className='mr-auto'>{item.title}</span>
-            </Link>
-          )
+              <span className="mr-auto">{item.title}</span>
+            </NavLink>
+          );
         })}
       </nav>
     </div>
-  )
+  );
 }
