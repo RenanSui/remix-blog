f9e754a9659f90d37f9ea079fa37b85df4f8a7f7
RenanSui <renanddtao@gmail.com>
2024-06-13 10:24:02 -0300

    modify -> cookies.server

--- app/cookies.server.ts
+++ app/cookies.server.ts
@@ -1,5 +1,9 @@
 import { createCookie } from '@remix-run/node' // or cloudflare/deno
 
-export const layoutCookie = createCookie('react-resizable-panels:layout')
+export const layoutCookie = createCookie('react-resizable-panels:layout', {
+  maxAge: 60 * 60 * 24 * 30 * 12 // 360 Days
+})
 
-export const collapsedCookie = createCookie('react-resizable-panels:collapsed')
+export const collapsedCookie = createCookie('react-resizable-panels:collapsed', {
+  maxAge: 60 * 60 * 24 * 30 * 12 // 360 Days
+})
