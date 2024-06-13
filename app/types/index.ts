e845e13f4ad5fc064dce339ca19952c0a73cd240
RenanSui <renanddtao@gmail.com>
2024-06-13 17:35:23 -0300

    extend -> types

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,15 +1,16 @@
-import type { Icons } from '@/components/icon'
+import type { Icons } from "@/components/icon";
+import type { ReasonPhrases } from 'http-status-codes'
 
 export type SidebarNavItem = {
-  title: string
-  href: string
-  icon?: keyof typeof Icons
-  disabled: boolean
-}
+  title: string;
+  href: string;
+  icon?: keyof typeof Icons;
+  disabled: boolean;
+};
 
 export type Sidebar = {
-  items: SidebarNavItem[]
-}
+  items: SidebarNavItem[];
+};
 
 export type Post = {
   id: string;
@@ -17,4 +18,14 @@ export type Post = {
   authorId: string;
   createdAt: Date;
   updatedAt: Date;
-}
+};
+
+export type ReasonPhrase = `${ReasonPhrases}`;
+
+export type HTTPResponse<T = void, B = void> = {
+  data: T | null;
+  message: ReasonPhrase;
+  status: B;
+};
+
+export type Auth = { accessToken: string }
