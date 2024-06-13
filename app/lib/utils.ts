aff30dcfddbbaae40aa629cad0639b76b735fa10
RenanSui <renanddtao@gmail.com>
2024-06-13 10:22:39 -0300

    extend -> lib utils

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -5,3 +5,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function serializeValue(value: any) {
+  return btoa(JSON.stringify({ value }));
+}
