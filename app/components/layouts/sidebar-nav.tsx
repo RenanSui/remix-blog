f960b628168c148587eefb1f9d08f633124f9bf5
RenanSui <renanddtao@gmail.com>
2024-06-14 09:33:22 -0300

    modify -> sidebar-nav: add tooltip

--- app/components/layouts/sidebar-nav.tsx
+++ app/components/layouts/sidebar-nav.tsx
@@ -3,6 +3,7 @@ import type { SidebarNavItem } from "@/types";
 import { NavLink } from "@remix-run/react";
 import { Icons } from "../icon";
 import { buttonVariants } from "../ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 export interface SidebarNavProps extends React.HTMLAttributes<HTMLDivElement> {
   items: SidebarNavItem[];
@@ -28,22 +29,28 @@ export function SidebarNav({
           const Icon = Icons[item.icon ?? "chevronLeft"];
 
           return isCollapsed ? (
-            <NavLink
-              aria-label={item.title}
-              key={index}
-              to={item.href}
-              className={({ isActive, isPending }) => {
-                return cn(
-                  buttonVariants({ variant: "ghost", size: "icon" }),
-                  "h-9 w-9",
-                  isActive ? "bg-accent text-accent-foreground" : "",
-                  item.disabled && "pointer-events-none opacity-60"
-                );
-              }}
-            >
-              <Icon className="h-4 w-4" />
-              <span className="sr-only">{item.title}</span>
-            </NavLink>
+            <Tooltip key={index} delayDuration={0}>
+              <NavLink
+                aria-label={item.title}
+                to={item.href}
+                className={({ isActive }) =>
+                  cn(
+                    buttonVariants({ variant: "ghost", size: "icon" }),
+                    "size-9",
+                    isActive ? "bg-accent text-accent-foreground" : "",
+                    item.disabled && "pointer-events-none opacity-60"
+                  )
+                }
+              >
+                <TooltipTrigger className="size-full flex items-center justify-center">
+                    <Icon className="h-4 w-4" />
+                    <span className="sr-only">{item.title}</span>
+                </TooltipTrigger>
+              </NavLink>
+              <TooltipContent side="right" className="flex items-center gap-4">
+                {item.title}
+              </TooltipContent>
+            </Tooltip>
           ) : (
             <NavLink
               aria-label={item.title}
