8fc2e8c0e416bbe4ccd6da913291645e08c27f77
RenanSui <renanddtao@gmail.com>
2024-06-14 09:32:44 -0300

    fix -> icons properties

--- app/components/icon.tsx
+++ app/components/icon.tsx
@@ -34,9 +34,9 @@ export const Icons = {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       <path d="M2.7 10.3a2.41 2.41 0 0 0 0 3.41l7.59 7.59a2.41 2.41 0 0 0 3.41 0l7.59-7.59a2.41 2.41 0 0 0 0-3.41l-7.59-7.59a2.41 2.41 0 0 0-3.41 0Z" />
