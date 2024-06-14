e9fc0955187ec1bc6dd455a7e6fa25407a8fe650
RenanSui <renanddtao@gmail.com>
2024-06-14 10:05:49 -0300

    create profile route

---
+++ app/routes/_profile+/profile.tsx
@@ -0,0 +1,14 @@
+import { profile } from "@/lib/actions/profile";
+import { getCookie } from "@/lib/utils";
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const cookieHeader = request.headers.get("Cookie") ?? "";
+  const accessToken = getCookie("accessToken", cookieHeader);
+  const Profile = accessToken ? await profile.getMe(accessToken) : null;
+
+  if (!Profile) return redirect("/signin");
+
+  return redirect(`/profile/${Profile.username}`);
+}
