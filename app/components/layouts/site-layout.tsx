524b2da937e9f9bae972b16985a573af2876c493
RenanSui <renanddtao@gmail.com>
2024-06-14 09:33:07 -0300

    modify -> site-layout: add tooltip provider

--- app/components/layouts/site-layout.tsx
+++ app/components/layouts/site-layout.tsx
@@ -9,6 +9,7 @@ import {
 } from "../ui/resizable";
 import { ScrollArea } from "../ui/scroll-area";
 import { Separator } from "../ui/separator";
+import { TooltipProvider } from "../ui/tooltip";
 import { AuthDropdown } from "./auth-dropdown";
 import { SidebarNav } from "./sidebar-nav";
 
@@ -31,66 +32,69 @@ export default function SiteLayout({
   const [isCollapsed, setIsCollapsed] = React.useState(defaultCollapsed);
 
   return (
-    <ResizablePanelGroup
-      direction="horizontal"
-      onLayout={(sizes: number[]) => {
-        document.cookie = `react-resizable-panels:layout=${serializeValue(
-          sizes
-        )}`;
-      }}
-      className="h-full items-stretch"
-    >
-      <ResizablePanel
-        defaultSize={defaultLayout[0]}
-        collapsedSize={navCollapsedSize}
-        collapsible={true}
-        minSize={15}
-        maxSize={20}
-        onCollapse={() => {
-          setIsCollapsed(true);
-          document.cookie = `react-resizable-panels:collapsed=${serializeValue(
-            true
+    <TooltipProvider delayDuration={0}>
+      <ResizablePanelGroup
+        direction="horizontal"
+        onLayout={(sizes: number[]) => {
+          document.cookie = `react-resizable-panels:layout=${serializeValue(
+            sizes
           )}`;
         }}
-        onExpand={() => {
-          setIsCollapsed(false);
-          document.cookie = `react-resizable-panels:collapsed=${serializeValue(
-            false
-          )}`;
-        }}
-        className={cn(
-          isCollapsed && "min-w-[50px] transition-all duration-300 ease-in-out"
-        )}
+        className="h-full items-stretch"
       >
-        <div
+        <ResizablePanel
+          defaultSize={defaultLayout[0]}
+          collapsedSize={navCollapsedSize}
+          collapsible={true}
+          minSize={15}
+          maxSize={20}
+          onCollapse={() => {
+            setIsCollapsed(true);
+            document.cookie = `react-resizable-panels:collapsed=${serializeValue(
+              true
+            )}`;
+          }}
+          onExpand={() => {
+            setIsCollapsed(false);
+            document.cookie = `react-resizable-panels:collapsed=${serializeValue(
+              false
+            )}`;
+          }}
           className={cn(
-            "flex h-[52px] items-center justify-center",
-            isCollapsed ? "h-[52px]" : "px-2"
+            isCollapsed &&
+              "min-w-[50px] transition-all duration-300 ease-in-out"
           )}
         >
-          <AuthDropdown profile={profile} isCollapsed={isCollapsed} />
-        </div>
-        <Separator />
-        <aside>
-          <ScrollArea className="h-[calc(100vh-3.3rem)]">
-            <SidebarNav
-              items={siteConfig.sidebarNav}
-              className="px-2 py-2"
-              isCollapsed={isCollapsed}
-            />
-            <Separator />
-            <SidebarNav
-              items={siteConfig.dashboardNav}
-              className="px-2 py-2"
-              isCollapsed={isCollapsed}
-            />
-          </ScrollArea>
-        </aside>
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
-        {page}
-      </ResizablePanel>
-    </ResizablePanelGroup>
+          <div
+            className={cn(
+              "flex h-[52px] items-center justify-center",
+              isCollapsed ? "h-[52px]" : "px-2"
+            )}
+          >
+            <AuthDropdown profile={profile} isCollapsed={isCollapsed} />
+          </div>
+          <Separator />
+          <aside>
+            <ScrollArea className="h-[calc(100vh-3.3rem)]">
+              <SidebarNav
+                items={siteConfig.sidebarNav}
+                className="px-2 py-2"
+                isCollapsed={isCollapsed}
+              />
+              <Separator />
+              <SidebarNav
+                items={siteConfig.dashboardNav}
+                className="px-2 py-2"
+                isCollapsed={isCollapsed}
+              />
+            </ScrollArea>
+          </aside>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
+          {page}
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </TooltipProvider>
   );
 }
