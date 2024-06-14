3ec839d3db67c859aab73333f87029e93d5287d3
RenanSui <renanddtao@gmail.com>
2024-06-14 07:47:56 -0300

    create -> user actions

---
+++ app/lib/actions/user.ts
@@ -0,0 +1,16 @@
+import type { HTTPResponse, User } from "@/types";
+import type { AuthStatusCode } from "../errors/handle-auth-error";
+
+export const user = {
+  getMe: async (accessToken: string) => {
+    const response = await fetch("http://localhost:8000/api/user/me", {
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const data = (await response.json()) as HTTPResponse<User, AuthStatusCode>;
+    return data.data
+  },
+};
