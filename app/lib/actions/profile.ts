f91cd1ebd1ea2fb612a3a0152a261224e7f7f7ed
RenanSui <renanddtao@gmail.com>
2024-06-14 15:49:23 -0300

    modify -> profile function name

--- app/lib/actions/profile.ts
+++ app/lib/actions/profile.ts
@@ -1,34 +1,34 @@
-import type { HTTPResponse, Profile } from "@/types";
-import type { AuthStatusCode } from "../errors/handle-auth-error";
+import type { HTTPResponse, Profile } from '@/types'
+import type { AuthStatusCode } from '../errors/handle-auth-error'
 
 export const profile = {
   getMe: async (accessToken: string) => {
-    const response = await fetch("http://localhost:8000/api/profile/me", {
-      credentials: "include",
+    const response = await fetch('http://localhost:8000/api/profile/me', {
+      credentials: 'include',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         authorization: `Bearer ${accessToken}`,
       },
-    });
+    })
     const { data } = (await response.json()) as HTTPResponse<
       Profile,
       AuthStatusCode
-    >;
-    return data;
+    >
+    return data
   },
 
-  getByProfileId: async (id: string) => {
+  getByUsername: async (id: string) => {
     const URL = `http://localhost:8000/api/profile/username/${id}`
     const response = await fetch(URL, {
-      credentials: "include",
+      credentials: 'include',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-    });
+    })
     const { data } = (await response.json()) as HTTPResponse<
       Profile,
       AuthStatusCode
-    >;
-    return data;
+    >
+    return data
   },
-};
+}
