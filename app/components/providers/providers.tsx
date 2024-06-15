22ab338bb8d32f1b0ed9890666d8d77de7b80af9
RenanSui <renanddtao@gmail.com>
2024-06-15 19:48:18 -0300

    create -> providers

---
+++ app/components/providers/providers.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { ReactQueryProvider } from './react-query-provider'
+import { ThemeProvider } from './theme-provider'
+
+export function Providers({ children }: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <ReactQueryProvider>
+      <ThemeProvider>{children}</ThemeProvider>
+    </ReactQueryProvider>
+  )
+}
