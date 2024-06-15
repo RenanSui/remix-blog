fe1ead220b533eee939a67d34b8a5d494195cb87
RenanSui <renanddtao@gmail.com>
2024-06-15 14:07:42 -0300

    modify -> index layout

--- app/routes/_index+/_layout.tsx
+++ app/routes/_index+/_layout.tsx
@@ -1,9 +1,12 @@
-import SiteLayout from '@/components/layouts/site-layout'
+import { SiteLayout } from '@/components/layouts/site-layout'
+import { SiteSidebar } from '@/components/layouts/site-sidebar'
+import { useMediaQuery } from '@/hooks/use-media-query'
 import { profile } from '@/lib/actions/profile'
 import { getSidebarCookies } from '@/lib/cookies'
 import { getCookie } from '@/lib/utils'
 import { json, type LoaderFunctionArgs } from '@remix-run/node'
 import { Outlet, useLoaderData } from '@remix-run/react'
+import * as React from 'react'
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const cookieHeader = request.headers.get('Cookie') ?? ''
@@ -18,15 +21,23 @@ export async function loader({ request }: LoaderFunctionArgs) {
 export default function Layout() {
   const data = useLoaderData<typeof loader>()
   const defaultLayout = data?.layout ? data.layout.value : undefined
-  const defaultCollapsed = data?.collapsed ? data.collapsed.value : undefined
+  const defaultCollapsed = data?.collapsed ? data.collapsed.value : false
+  const [isCollapsed, setIsCollapsed] = React.useState(defaultCollapsed)
+  const isTablet = useMediaQuery('(min-width: 768px)')
 
-  return (
+  return isTablet ? (
     <SiteLayout
-      page={<Outlet />}
-      profile={data.profile}
       defaultLayout={defaultLayout}
-      defaultCollapsed={defaultCollapsed}
       navCollapsedSize={4}
+      isCollapsed={isCollapsed}
+      setIsCollapsed={setIsCollapsed}
+      leftSidebar={
+        <SiteSidebar profile={data.profile} isCollapsed={isCollapsed} />
+      }
+      page={<Outlet />}
+      rightSidebar={<div>Oi</div>}
     />
+  ) : (
+    <Outlet />
   )
 }
