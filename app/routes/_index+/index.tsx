f646d00c936222392276906cd3ec82f59d0d4ef0
RenanSui <renanddtao@gmail.com>
2024-06-15 14:01:39 -0300

    modify -> index page

--- app/routes/_index+/index.tsx
+++ app/routes/_index+/index.tsx
@@ -1,4 +1,5 @@
 import { Icons } from '@/components/icon'
+import { PageHeader, PageHeaderHeading } from '@/components/page-header'
 import { Input } from '@/components/ui/input'
 import { Separator } from '@/components/ui/separator'
 import type { MetaFunction } from '@remix-run/node'
@@ -12,23 +13,22 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
-      <header></header>
-      <main className="max-w-[512px]">
-        <div className="flex items-center px-4 py-3">
-          <h1 className="text-xl font-bold">Home</h1>
-        </div>
-        <Separator />
-        <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-          <form>
-            <div className="relative">
-              {/* eslint-disable-next-line react/jsx-pascal-case */}
-              <Icons.search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input placeholder="Search" className="pl-8" />
-            </div>
-          </form>
-        </div>
-      </main>
-    </div>
+    <main>
+      <PageHeader as="header" className="px-4 py-[15px]">
+        <PageHeaderHeading className="text-xl md:text-xl">
+          Home
+        </PageHeaderHeading>
+      </PageHeader>
+      <Separator />
+      <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <form>
+          <div className="relative">
+            {/* eslint-disable-next-line react/jsx-pascal-case */}
+            <Icons.search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input placeholder="Search" className="pl-8" />
+          </div>
+        </form>
+      </div>
+    </main>
   )
 }
