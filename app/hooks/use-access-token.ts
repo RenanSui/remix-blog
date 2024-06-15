b22fb39c933936f2da025bcd11058b7526682301
RenanSui <renanddtao@gmail.com>
2024-06-15 20:52:02 -0300

    modify -> use access token type

--- app/hooks/use-access-token.ts
+++ app/hooks/use-access-token.ts
@@ -1,7 +1,6 @@
-import { Auth } from '@/types'
 import { atom, useAtom } from 'jotai'
 
-type Config = Auth['accessToken'] | null
+type Config = string | null
 
 export const accessTokenAtom = atom<Config>(null)
 
