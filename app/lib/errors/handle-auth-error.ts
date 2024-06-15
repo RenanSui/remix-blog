bdefb860c0a176b1fa2084e13533a1d760e4817c
RenanSui <renanddtao@gmail.com>
2024-06-15 13:58:19 -0300

    modify -> http error messages

--- app/lib/errors/handle-auth-error.ts
+++ app/lib/errors/handle-auth-error.ts
@@ -2,10 +2,10 @@ import { StatusCodes } from 'http-status-codes'
 
 export const HTPPErrorMessages = {
   200: 'The request has succeeded.',
-  400: 'The server could not understand the request due to invalid syntax.',
+  400: 'The information sent is incorrect.',
   401: 'the user must authenticate to use the services.',
   403: 'The user does not have access rights to the content.',
-  404: 'User not found. Please create an account.',
+  404: 'User not found.',
   409: 'User already exist.',
 }
 
