3ffb03f5782256141d82b43f9ff9479dc1026884
RenanSui <renanddtao@gmail.com>
2024-06-17 08:25:50 -0300

    extend & modify -> cookies server

--- app/cookies.server.ts
+++ app/cookies.server.ts
@@ -2,11 +2,31 @@ import { createCookie } from '@remix-run/node' // or cloudflare/deno
 
 export const layoutCookie = createCookie('react-resizable-panels:layout', {
   maxAge: 60 * 60 * 24 * 30 * 12, // 360 Days
+  path: '/',
 })
 
 export const collapsedCookie = createCookie(
   'react-resizable-panels:collapsed',
   {
     maxAge: 60 * 60 * 24 * 30 * 12, // 360 Days
+    path: '/',
   },
 )
+
+type Layout = { value: number[] | null }
+type Collapsed = { value: boolean | null }
+export async function getSidebarCookies(cookieHeader: string | null) {
+  const layout: Layout = await layoutCookie.parse(cookieHeader)
+  const collapsed: Collapsed = await collapsedCookie.parse(cookieHeader)
+
+  const sidebarCookies = { layout, collapsed }
+  const headers: HeadersInit | undefined = [
+    ['Set-Cookie', await layoutCookie.serialize({ value: [20, 80] })],
+    ['Set-Cookie', await collapsedCookie.serialize({ value: false })],
+  ]
+
+  return {
+    sidebarCookies,
+    headers: !layout || !collapsed ? headers : undefined,
+  }
+}
