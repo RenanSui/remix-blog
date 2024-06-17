15834cd12a9162b25c9ca53371d1b3abcd87149a
RenanSui <renanddtao@gmail.com>
2024-06-17 08:30:41 -0300

    modify -> index layout

--- app/routes/_index+/_layout.tsx
+++ app/routes/_index+/_layout.tsx
@@ -1,11 +1,13 @@
 import { SiteLayout } from '@/components/layouts/site-layout'
 import { SiteSidebar } from '@/components/layouts/site-sidebar'
-import { useMediaQuery } from '@/hooks/use-media-query'
+import { getSidebarCookies } from '@/cookies.server'
+import { accessTokenAtom } from '@/hooks/use-access-token'
+import { useMounted } from '@/hooks/use-mounted'
 import { profile } from '@/lib/actions/profile'
-import { getSidebarCookies } from '@/lib/cookies'
 import { getCookie } from '@/lib/utils'
 import { json, type LoaderFunctionArgs } from '@remix-run/node'
 import { Outlet, useLoaderData } from '@remix-run/react'
+import { useHydrateAtoms } from 'jotai/utils'
 import * as React from 'react'
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -15,17 +17,22 @@ export async function loader({ request }: LoaderFunctionArgs) {
   const accessToken = getCookie('accessToken', cookieHeader)
   const Profile = accessToken ? await profile.getMe(accessToken) : null
 
-  return json({ ...sidebarCookies, profile: Profile }, { headers })
+  return json({ ...sidebarCookies, profile: Profile, accessToken }, { headers })
 }
 
 export default function Layout() {
   const data = useLoaderData<typeof loader>()
-  const defaultLayout = data?.layout ? data.layout.value : undefined
-  const defaultCollapsed = data?.collapsed ? data.collapsed.value : false
+  const defaultLayout = data?.layout?.value || undefined
+  const defaultCollapsed = data?.collapsed?.value || false
   const [isCollapsed, setIsCollapsed] = React.useState(defaultCollapsed)
-  const isTablet = useMediaQuery('(min-width: 768px)')
 
-  return isTablet ? (
+  const mounted = useMounted()
+  // const isTablet = useMediaQuery('(min-width: 768px)')
+  // const [post] = usePost()
+
+  useHydrateAtoms([[accessTokenAtom, data.accessToken]])
+
+  return mounted ? (
     <SiteLayout
       defaultLayout={defaultLayout}
       navCollapsedSize={4}
@@ -35,9 +42,15 @@ export default function Layout() {
         <SiteSidebar profile={data.profile} isCollapsed={isCollapsed} />
       }
       page={<Outlet />}
-      rightSidebar={<div>Oi</div>}
+      rightSidebar={
+        <div></div>
+        // <PostDisplay
+        //   profile={data.profile}
+        //   post={posts.find((item) => item.id === post.selected)}
+        // />
+      }
     />
   ) : (
-    <Outlet />
+    <div></div>
   )
 }
