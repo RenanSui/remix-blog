4b5add02d10e22d04213d0e0b6db65db44c68c95
RenanSui <renanddtao@gmail.com>
2024-06-17 17:20:38 -0300

    create -> settings page

---
+++ app/routes/_index+/settings+/index.tsx
@@ -0,0 +1,42 @@
+import { PageHeader, PageHeaderHeading } from '@/components/page-header'
+import { ProfileForm } from '@/components/profile-form'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Separator } from '@/components/ui/separator'
+import { profileService } from '@/lib/actions/profile'
+import { getCookie } from '@/lib/utils'
+import { LoaderFunctionArgs, json, redirect } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const cookieHeader = request.headers.get('Cookie') ?? ''
+  const accessToken = getCookie('accessToken', cookieHeader)
+  if (!accessToken) return redirect('/signin')
+
+  const profile = accessToken ? await profileService.getMe(accessToken) : null
+  if (!profile) return redirect('/signin')
+
+  return json({ profile })
+}
+
+export default function SettingsIndex() {
+  const { profile } = useLoaderData<typeof loader>()
+
+  return (
+    <main>
+      <ScrollArea className="overflow-y-auto h-screen">
+        <PageHeader
+          as="header"
+          className="px-4 py-[15px] sticky border-b top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+        >
+          <PageHeaderHeading className="text-xl md:text-xl">
+            Settings
+          </PageHeaderHeading>
+        </PageHeader>
+        <Separator />
+        <div className="p-4">
+          <ProfileForm profile={profile} />
+        </div>
+      </ScrollArea>
+    </main>
+  )
+}
