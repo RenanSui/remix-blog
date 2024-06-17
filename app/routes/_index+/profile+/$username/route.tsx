f5806f9880f0fe287380eee98dae74850f8b1773
RenanSui <renanddtao@gmail.com>
2024-06-17 08:37:49 -0300

    modify -> profile username route

--- app/routes/_index+/profile+/$username/route.tsx
+++ app/routes/_index+/profile+/$username/route.tsx
@@ -1,4 +1,5 @@
 import { PageHeader, PageHeaderHeading } from '@/components/page-header'
+import { PostList } from '@/components/post-list'
 import {
   Profile,
   ProfileBanner,
@@ -6,10 +7,13 @@ import {
   ProfileHeader,
 } from '@/components/profile'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
+import { postService } from '@/lib/actions/post'
 import { profile } from '@/lib/actions/profile'
 import { HTPPErrorMessages } from '@/lib/errors/handle-auth-error'
+import { sortPostsByDate } from '@/lib/utils'
+import { Post } from '@/types'
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
 import { json, redirect, useLoaderData } from '@remix-run/react'
 
@@ -17,8 +21,8 @@ export async function loader({ params }: LoaderFunctionArgs) {
   const username = params.username
   if (!username) return redirect('/')
 
-  const { data, status } = await profile.getByUsername(username)
-  if (!data) {
+  const { data: userProfile, status } = await profile.getByUsername(username)
+  if (!userProfile) {
     const message = HTPPErrorMessages[status]
     throw new Response(null, {
       status: 404,
@@ -26,9 +30,10 @@ export async function loader({ params }: LoaderFunctionArgs) {
     })
   }
 
-  // const posts = posts.getByUserId
+  const { data: posts } = await postService.getByUserId(userProfile.userId)
+  const sortedPosts = sortPostsByDate(posts) || []
 
-  return json({ profile: data })
+  return json({ profile: userProfile, posts: sortedPosts })
 }
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
@@ -45,38 +50,45 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
 }
 
 export default function ProfilePage() {
-  const { profile } = useLoaderData<typeof loader>()
+  const { profile, posts } = useLoaderData<typeof loader>()
 
   return profile ? (
     <main>
-      <PageHeader as="header" className="px-4 py-[15px]">
-        <PageHeaderHeading className="text-xl md:text-xl">
-          Profile
-        </PageHeaderHeading>
-      </PageHeader>
-      <Separator />
-      <div className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <Profile>
-          <ProfileBanner />
-          <ProfileHeader className="-mt-16 mx-4">
-            <Avatar className="size-32 border-4 border-background cursor-pointer">
-              <AvatarImage className="size-32" src={profile.image} />
-              <AvatarFallback />
-            </Avatar>
-            <h2 className="font-bold text-xl -mb-1">{profile.name}</h2>
-            <ProfileDescription variant="muted">
-              @{profile.username}
-            </ProfileDescription>
-            <ProfileDescription className="py-4" size="sm">
-              {profile.bio}
-              Freelance Artist│Acid Bufferzone, SILENT HILL 1/2/3/Short
-              Message_Art direction/creature, environment design, etc.│Prohibit
-              the use of contents for AI training
-            </ProfileDescription>
-          </ProfileHeader>
-        </Profile>
-        <pre className="p-4">{JSON.stringify(profile, null, 2)}</pre>
-      </div>
+      <ScrollArea className="overflow-y-auto h-screen">
+        <PageHeader
+          as="header"
+          className="px-4 py-[15px] sticky border-b top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+        >
+          <PageHeaderHeading className="text-xl md:text-xl">
+            {profile.name}&apos;s Profile
+          </PageHeaderHeading>
+        </PageHeader>
+        <Separator />
+        <div className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+          <Profile>
+            <ProfileBanner />
+            <ProfileHeader className="-mt-16 mx-4">
+              <Avatar className="size-32 border-4 border-background cursor-pointer">
+                <AvatarImage className="size-32" src={profile.image} />
+                <AvatarFallback>
+                  {profile.name
+                    .split(' ')
+                    .map((chunk) => chunk[0])
+                    .join('')}
+                </AvatarFallback>
+              </Avatar>
+              <h2 className="font-bold text-xl -mb-1">{profile.name}</h2>
+              <ProfileDescription variant="muted">
+                @{profile.username}
+              </ProfileDescription>
+              <ProfileDescription className="py-4" size="sm">
+                {profile.bio}
+              </ProfileDescription>
+            </ProfileHeader>
+          </Profile>
+          <PostList items={posts as unknown as Post[]} />
+        </div>
+      </ScrollArea>
     </main>
   ) : null
 }
