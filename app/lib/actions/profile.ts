01ed0453b98f4e4cefc658f7776c30ffb49e4955
RenanSui <renanddtao@gmail.com>
2024-06-17 15:30:18 -0300

    extend -> profile actions: add update

--- app/lib/actions/profile.ts
+++ app/lib/actions/profile.ts
@@ -1,7 +1,8 @@
 import type { HTTPResponse, Profile } from '@/types'
 import type { AuthStatusCode } from '../errors/handle-auth-error'
+import { UpdateProfileSchema } from '../validations/profile'
 
-export const profile = {
+export const profileService = {
   getMe: async (accessToken: string) => {
     const response = await fetch('http://localhost:8000/api/profile/me', {
       credentials: 'include',
@@ -37,5 +38,25 @@ export const profile = {
       },
     })
     return (await response.json()) as HTTPResponse<Profile, AuthStatusCode>
+  },
+
+  update: async (
+    formData: UpdateProfileSchema & { id?: string; userId?: string },
+    accessToken: string,
+  ) => {
+    const response = await fetch('http://localhost:8000/api/profile/update', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ ...formData }),
+    })
+    const { data } = (await response.json()) as HTTPResponse<
+      Profile,
+      AuthStatusCode
+    >
+    return data
   },
 }
