77272226c3eed1ec7507c43a5e2c0dbe16651820
RenanSui <renanddtao@gmail.com>
2024-06-18 10:40:57 -0300

    fix -> imports

--- app/components/layouts/theme-toggler.tsx
+++ app/components/layouts/theme-toggler.tsx
@@ -7,14 +7,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import {
-  CaretSortIcon,
-  LaptopIcon,
-  MoonIcon,
-  SunIcon,
-} from '@radix-ui/react-icons'
 import { useTheme } from 'next-themes'
 import { useEffect } from 'react'
+import { Icons } from '../icon'
 
 export const ThemeToggler = ({
   labelled,
@@ -40,10 +35,10 @@ export const ThemeToggler = ({
             className="w-fit justify-between p-1 py-0"
           >
             <div className="flex items-center gap-1 capitalize">
-              <SunIcon className="h-3 w-3 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <MoonIcon className="absolute h-3 w-3 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <Icons.sun className="h-3 w-3 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Icons.moon className="absolute h-3 w-3 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
               {theme}
-              <CaretSortIcon className="h-4 w-4 shrink-0 opacity-50" />
+              <Icons.caretSortIcon className="h-4 w-4 shrink-0 opacity-50" />
             </div>
           </Button>
         </DropdownMenuTrigger>
@@ -52,18 +47,18 @@ export const ThemeToggler = ({
             role="light-toggle"
             onClick={() => setTheme('light')}
           >
-            <SunIcon className="mr-2 h-3 w-3" />
+            <Icons.sun className="mr-2 h-3 w-3" />
             <span>Light</span>
           </DropdownMenuItem>
           <DropdownMenuItem role="dark-toggle" onClick={() => setTheme('dark')}>
-            <MoonIcon className="mr-2 h-3 w-3" />
+            <Icons.moon className="mr-2 h-3 w-3" />
             <span>Dark</span>
           </DropdownMenuItem>
           <DropdownMenuItem
             role="system-toggle"
             onClick={() => setTheme('system')}
           >
-            <LaptopIcon className="mr-2 h-3 w-3" />
+            <Icons.laptopIcon className="mr-2 h-3 w-3" />
             <span>System</span>
           </DropdownMenuItem>
         </DropdownMenuContent>
@@ -80,8 +75,8 @@ export const ThemeToggler = ({
           aria-label="theme-toggler"
         >
           <span className="item-center flex gap-2 capitalize">
-            <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <Icons.sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Icons.moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             {labelled ? theme : null}
           </span>
           <span className="sr-only">Toggle theme</span>
@@ -89,18 +84,18 @@ export const ThemeToggler = ({
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem role="light-toggle" onClick={() => setTheme('light')}>
-          <SunIcon className="mr-2 h-4 w-4" />
+          <Icons.sun className="mr-2 h-4 w-4" />
           <span>Light</span>
         </DropdownMenuItem>
         <DropdownMenuItem role="dark-toggle" onClick={() => setTheme('dark')}>
-          <MoonIcon className="mr-2 h-4 w-4" />
+          <Icons.moon className="mr-2 h-4 w-4" />
           <span>Dark</span>
         </DropdownMenuItem>
         <DropdownMenuItem
           role="system-toggle"
           onClick={() => setTheme('system')}
         >
-          <LaptopIcon className="mr-2 h-4 w-4" />
+          <Icons.laptopIcon className="mr-2 h-4 w-4" />
           <span>System</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
