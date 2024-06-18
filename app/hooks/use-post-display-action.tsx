9c35219703e386d9278b4c7735bb72986203c307
RenanSui <renanddtao@gmail.com>
2024-06-18 10:36:02 -0300

    modify & create -> type for post display action

--- app/hooks/use-post-display-action.tsx
+++ app/hooks/use-post-display-action.tsx
@@ -1,6 +1,8 @@
 import { atom, useAtom } from 'jotai'
 
-const configAtom = atom<'post' | 'add' | 'update'>('post')
+export type DisplayAction = 'post' | 'add' | 'update'
+
+const configAtom = atom<DisplayAction>('post')
 
 export function usePostDisplayAction() {
   return useAtom(configAtom)
