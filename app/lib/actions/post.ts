4e380a288d44c8713ee42c164d58b6c525a6878d
RenanSui <renanddtao@gmail.com>
2024-06-18 12:20:44 -0300

    extend & modify -> post actions

--- app/lib/actions/post.ts
+++ app/lib/actions/post.ts
@@ -1,6 +1,6 @@
 import { HTTPResponse, Post } from '@/types'
 import type { z } from 'zod'
-import { AuthStatusCode } from '../errors/handle-auth-error'
+import { PostStatusCode } from '../errors/handle-post-error'
 import { createPostSchema } from '../validations/post'
 
 type Inputs = z.infer<typeof createPostSchema>
@@ -21,17 +21,29 @@ export const postService = {
       },
       body: JSON.stringify({ ...formData }),
     })
-    return (await response.json()) as HTTPResponse<Post, AuthStatusCode>
+    return (await response.json()) as HTTPResponse<Post, PostStatusCode>
   },
 
   getById: async (postId: string) => {
     const response = await fetch(`http://localhost:8000/api/post/id/${postId}`)
-    return (await response.json()) as HTTPResponse<Post, AuthStatusCode>
+    return (await response.json()) as HTTPResponse<Post, PostStatusCode>
   },
 
   getByUserId: async (userId: string) => {
     const URL = `http://localhost:8000/api/post/user/${userId}`
     const response = await fetch(URL)
-    return (await response.json()) as HTTPResponse<Post[], AuthStatusCode>
+    return (await response.json()) as HTTPResponse<Post[], PostStatusCode>
+  },
+
+  update: async (formData: Partial<Post>, accessToken: string) => {
+    const response = await fetch('http://localhost:8000/api/post/update', {
+      ...postFetchConfig,
+      headers: {
+        ...postFetchConfig.headers,
+        authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ ...formData }),
+    })
+    return (await response.json()) as HTTPResponse<Post, PostStatusCode>
   },
 }
