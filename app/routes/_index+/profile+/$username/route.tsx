cb8054279e742010efe3729043f90b71c44de0f5
RenanSui <renanddtao@gmail.com>
2024-06-19 18:52:41 -0300

    update -> username route to handle infinite scroll

--- app/routes/_index+/profile+/$username/route.tsx
+++ app/routes/_index+/profile+/$username/route.tsx
@@ -1,3 +1,4 @@
+import { Icons } from '@/components/icon'
 import { PageHeader, PageHeaderHeading } from '@/components/page-header'
 import { PostList } from '@/components/post-list'
 import {
@@ -9,13 +10,14 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
+import { Skeleton } from '@/components/ui/skeleton'
 import { usePostByUserId } from '@/hooks/post'
 import { ProfileService } from '@/lib/actions/profile'
 import { HTPPErrorMessages } from '@/lib/errors/handle-auth-error'
-import { sortPostsByDate } from '@/lib/utils'
-import { Post } from '@/types'
+import { debounce } from '@/lib/utils'
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
 import { json, redirect, useLoaderData } from '@remix-run/react'
+import * as React from 'react'
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const username = params.username
@@ -34,24 +36,37 @@ export async function loader({ params }: LoaderFunctionArgs) {
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
   const profile = data?.profile
+  const title = `${profile?.name} (@${profile?.username}) / Blog`
 
   return [
-    {
-      title: profile
-        ? `${profile.name} (@${profile.username}) / Blog`
-        : 'Profile',
-    },
+    { title: profile ? title : 'Profile' },
     { name: 'description', content: 'Simple Blog built with Remix.' },
   ]
 }
 
 export default function ProfilePage() {
+  const scrollRef = React.useRef<HTMLDivElement>(null)
   const { profile } = useLoaderData<typeof loader>()
-  const posts = sortPostsByDate(usePostByUserId(profile?.userId).data?.data)
+  const { data, ...reactQuery } = usePostByUserId(profile?.userId, 0, 7)
+
+  const handleScroll = debounce((e: Event) => {
+    const { scrollTop, scrollHeight, clientHeight } = e.target as HTMLElement
+    if (clientHeight + scrollTop + scrollHeight / 3 >= scrollHeight) {
+      reactQuery.fetchNextPage()
+    }
+  }, 500)
+
+  React.useEffect(() => {
+    if (!scrollRef.current) return
+
+    const main = scrollRef.current
+    main.addEventListener('scroll', handleScroll)
+    return () => main.removeEventListener('scroll', handleScroll)
+  }, [handleScroll])
 
   return profile ? (
     <main>
-      <ScrollArea className="overflow-y-auto h-screen">
+      <ScrollArea temporaryRef={scrollRef} className="overflow-y-auto h-screen">
         <PageHeader
           as="header"
           className="px-4 py-[15px] sticky border-b top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
@@ -83,7 +98,34 @@ export default function ProfilePage() {
               </ProfileDescription>
             </ProfileHeader>
           </Profile>
-          {posts && <PostList items={posts as unknown as Post[]} />}
+
+          {data?.pages.map(
+            (posts, index) =>
+              posts.data && <PostList key={index} items={posts.data} />,
+          )}
+
+          {!reactQuery.hasNextPage && !reactQuery.isFetchingNextPage && (
+            <div className="relative mb-4">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  End of Posts
+                </span>
+              </div>
+            </div>
+          )}
+
+          {reactQuery.isFetchingNextPage &&
+            Array.from({ length: 3 }).map((_, index) => (
+              <Skeleton
+                key={index}
+                className="border mx-4 mb-4 py-1 cursor-pointer"
+              >
+                <Icons.spinner className="mx-auto animate-spin size-8 shrink-0 my-4" />
+              </Skeleton>
+            ))}
         </div>
       </ScrollArea>
     </main>
