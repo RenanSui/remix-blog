75e9badca7e2eeb89d5fd7fd63344c44eb2612d7
RenanSui <renanddtao@gmail.com>
2024-06-19 18:49:39 -0300

    extend -> lib utils

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -45,3 +45,15 @@ export function sortPostsByDate(posts: Post[] | null | undefined) {
         .reverse()
     : null
 }
+
+export function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(
+  func: F,
+  waitFor: number,
+): (...args: Parameters<F>) => void {
+  let timeout: ReturnType<typeof setTimeout>
+
+  return (...args: Parameters<F>): void => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => func(...args), waitFor)
+  }
+}
