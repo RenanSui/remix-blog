52a3575ec43b40ba19892f88bb1737be90139f66
RenanSui <renanddtao@gmail.com>
2024-06-20 17:09:02 -0300

    add vercelPreset

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from '@remix-run/dev'
 import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { vercelPreset } from '@vercel/remix/vite'
 
 export default defineConfig({
   plugins: [
@@ -22,6 +23,7 @@ export default defineConfig({
           ],
         })
       },
+      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],
