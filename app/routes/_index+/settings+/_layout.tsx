467f939ace25c4b0ab5b2044e7aeaeccbd482444
RenanSui <renanddtao@gmail.com>
2024-06-20 13:07:19 -0300

    check user on layout loader

--- app/routes/_index+/settings+/_layout.tsx
+++ app/routes/_index+/settings+/_layout.tsx
@@ -1,6 +1,21 @@
 import { SettingsSkeleton } from '@/components/loadings/settings-skeleton'
 import { useMounted } from '@/hooks/use-mounted'
-import { Outlet } from '@remix-run/react'
+import { ProfileService } from '@/lib/actions/profile'
+import { getCookie } from '@/lib/utils'
+import { LoaderFunctionArgs } from '@remix-run/node'
+import { Outlet, redirect } from '@remix-run/react'
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const cookieHeader = request.headers.get('Cookie') ?? ''
+  const accessToken = getCookie('accessToken', cookieHeader)
+  if (!accessToken) return redirect('/signin')
+
+  const profileService = new ProfileService(process.env.SERVER_URL)
+  const data = await profileService.getMe(accessToken)
+  if (!data?.data) return redirect('/signin')
+
+  return null
+}
 
 export default function Layout() {
   const mounted = useMounted()
