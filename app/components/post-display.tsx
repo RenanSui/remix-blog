33a3ca592de6297c1dd7b898249c91206534090b
RenanSui <renanddtao@gmail.com>
2024-06-20 11:41:29 -0300

    modify -> better post display component, add loading ui

--- app/components/post-display.tsx
+++ app/components/post-display.tsx
@@ -1,17 +1,21 @@
-import { usePostAtom, usePostById } from '@/hooks/post'
+import { usePostAtom } from '@/hooks/post'
 import {
   DisplayAction,
   usePostDisplayAction,
 } from '@/hooks/use-post-display-action'
 import { useProfileByUserId } from '@/hooks/use-profile'
+import { useSeverURLAtom } from '@/hooks/use-server-url'
+import { PostService } from '@/lib/actions/post'
 import { cn } from '@/lib/utils'
 import { Post, Profile } from '@/types'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@radix-ui/react-tabs'
-import { Link } from '@remix-run/react'
+import { Await, Link } from '@remix-run/react'
 import { format } from 'date-fns/format'
+import * as React from 'react'
 import { AddNewPost } from './add-new-post'
 import { DeletePost } from './delete-post'
 import { Icons } from './icon'
+import { PostDisplaySkeleton } from './loadings/post-display-skeleton'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { buttonVariants } from './ui/button'
 import { Separator } from './ui/separator'
@@ -23,10 +27,55 @@ interface PostDisplayProps {
 }
 
 export function PostDisplay({ profile }: PostDisplayProps) {
+  const [{ selected }] = usePostAtom()
+  const [serverURL] = useSeverURLAtom()
+  const postService = new PostService(serverURL)
+  const post = selected ? postService.getById(selected) : null
+
+  return (
+    <React.Suspense fallback={<PostDisplaySkeleton />}>
+      <Await
+        resolve={post}
+        errorElement={
+          <p className="p-8 text-center">Error loading post display</p>
+        }
+      >
+        {(data) =>
+          data?.data ? (
+            // eslint-disable-next-line no-use-before-define
+            <PostDisplayHeader post={data?.data} profile={profile}>
+              <TabsContent value="post">
+                <PostDisplayItem post={data.data} />
+              </TabsContent>
+              <TabsContent value="add">
+                <PostDisplayAdd profile={profile} />
+              </TabsContent>
+              <TabsContent value="update">
+                <PostDisplayUpdate post={data.data} profile={profile} />
+              </TabsContent>
+            </PostDisplayHeader>
+          ) : (
+            // eslint-disable-next-line no-use-before-define
+            <PostDisplayHeader post={null} profile={profile}>
+              <p className="p-8 text-center text-muted-foreground">
+                No message selected
+              </p>
+            </PostDisplayHeader>
+          )
+        }
+      </Await>
+    </React.Suspense>
+  )
+}
+
+type PostDisplayHeader = React.HTMLAttributes<HTMLDivElement> & {
+  post: Post | null | undefined
+  profile: Profile | null
+}
+
+function PostDisplayHeader({ post, profile, children }: PostDisplayHeader) {
   const [postId, setPost] = usePostAtom()
   const [action, setAction] = usePostDisplayAction()
-  const post = usePostById(postId.selected)?.data?.data
-
   return (
     <Tabs
       className="flex h-full flex-col"
@@ -35,18 +84,20 @@ export function PostDisplay({ profile }: PostDisplayProps) {
       onValueChange={(value) => setAction(value as DisplayAction)}
     >
       <TabsList className="p-2 gap-2 flex items-center">
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <TabsTrigger
-              value="add"
-              className={buttonVariants({ variant: 'ghost', size: 'icon' })}
-            >
-              <Icons.add className="h-4 w-4" />
-              <span className="sr-only"></span>
-            </TabsTrigger>
-          </TooltipTrigger>
-          <TooltipContent>Add New</TooltipContent>
-        </Tooltip>
+        {profile && (
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <TabsTrigger
+                value="add"
+                className={buttonVariants({ variant: 'ghost', size: 'icon' })}
+              >
+                <Icons.add className="h-4 w-4" />
+                <span className="sr-only"></span>
+              </TabsTrigger>
+            </TooltipTrigger>
+            <TooltipContent>Add New</TooltipContent>
+          </Tooltip>
+        )}
         {post?.authorId === profile?.userId && (
           <Tooltip>
             <TooltipTrigger asChild>
@@ -95,41 +146,41 @@ export function PostDisplay({ profile }: PostDisplayProps) {
         </Tooltip>
       </TabsList>
       <Separator />
-      <TabsContent value="post">
-        {postId.selected && post ? (
-          <PostItem post={post} />
-        ) : (
-          <p className="p-8 text-center text-muted-foreground">
-            No message selected
-          </p>
-        )}
-      </TabsContent>
-      <TabsContent value="add">
-        {profile ? (
-          <div className="p-4">
-            <AddNewPost />
-          </div>
-        ) : (
-          <div className="p-8 flex flex-col items-center gap-4">
-            Sign in to start posting.
-            <Link to="/signin" className={cn(buttonVariants({ size: 'sm' }))}>
-              Sign In
-            </Link>
-          </div>
-        )}
-      </TabsContent>
-      <TabsContent value="update">
-        {profile && post && (
-          <div className="p-4">
-            <UpdatePost post={post} />
-          </div>
-        )}
-      </TabsContent>
+      {children}
     </Tabs>
   )
 }
 
-const PostItem = ({ post }: { post: Post }) => {
+function PostDisplayAdd({ profile }: Pick<PostDisplayHeader, 'profile'>) {
+  return profile ? (
+    <div className="p-4">
+      <AddNewPost />
+    </div>
+  ) : (
+    <div className="p-8 flex flex-col items-center gap-4">
+      Sign in to start posting.
+      <Link to="/signin" className={cn(buttonVariants({ size: 'sm' }))}>
+        Sign In
+      </Link>
+    </div>
+  )
+}
+
+function PostDisplayUpdate({
+  post,
+  profile,
+}: Pick<PostDisplayHeader, 'post' | 'profile'>) {
+  return (
+    profile &&
+    post && (
+      <div className="p-4">
+        <UpdatePost post={post} />
+      </div>
+    )
+  )
+}
+
+const PostDisplayItem = ({ post }: { post: Post }) => {
   const profile = useProfileByUserId(post.authorId)?.data?.data
 
   if (!profile) {
