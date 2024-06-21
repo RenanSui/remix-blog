3e3663fe16c99f69ad552bac40c12ce40ebf77cf
RenanSui <renanddtao@gmail.com>
2024-06-21 16:18:32 -0300

    update auth validations with zod-form-data

--- app/lib/validations/auth.ts
+++ app/lib/validations/auth.ts
@@ -1,15 +1,12 @@
 import * as z from 'zod'
+import * as zfd from 'zod-form-data'
 
-export const authSchema = z.object({
-  email: z.string().email({
-    message: 'Please enter a valid email address',
-  }),
+export const authSchema = zfd.formData({
+  email: z.string().email({ message: 'Please enter a valid email address' }),
   password: z
     .string()
-    .min(8, {
-      message: 'Password must be at least 8 characters long',
-    })
-    .max(100, {
-      message: 'Password must be at most 100 characters long',
-    }),
+    .min(8, { message: 'Password must be at least 8 characters long' })
+    .max(100, { message: 'Password must be at most 100 characters long' }),
 })
+
+export type AuthSchema = z.infer<typeof authSchema>
